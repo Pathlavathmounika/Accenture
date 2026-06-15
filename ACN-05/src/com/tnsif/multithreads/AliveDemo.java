d62ac55359632ff4deacfe197131b75d2876614e
pathl <pathl@DESKTOP-57AJJDT>
2026-06-15 22:01:51 +0530

    Added today programs

---
+++ ACN-05/src/com/tnsif/multithreads/AliveDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.multithreads;
+
+public class AliveDemo extends Thread {
+	public void run() {
+		System.out.println("Thread Running");
+		
+	}
+
+	public static void main(String[] args) {
+		AliveDemo t = new AliveDemo();
+		System.out.println(t.isAlive());
+		t.start();
+		System.out.println(t.isAlive());
+		
+
+	}
+
+}
---
+++ ACN-05/src/com/tnsif/multithreads/SleepMain.java
@@ -0,0 +1,12 @@
+package com.tnsif.multithreads;
+
+public class SleepMain {
+
+	public static void main(String[] args) {
+		
+		SleepThread st = new SleepThread();
+		st.start();
+
+	}
+
+}
